dbb998bccb1a3f1e02137841d4a9bb7974989352
Marshmallow <xiongnemo@126.com>
2019-10-21 16:02:09 +0800

    hello, problem 1

---
+++ 1/1.cc
@@ -0,0 +1,10 @@
+#include "1.h"
+
+int main(void)
+{
+    int a = 0;
+    int b = 0;
+    
+    printf("hello, world");
+}
+
---
+++ 1/1.h
@@ -0,0 +1,18 @@
+#ifndef LINK_LIST_H
+#define LINK_LIST_H
+
+#include <cstdio>
+#include <cstring>
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
